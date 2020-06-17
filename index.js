7fdf0b436144cd9c215743e73e0a86b2ff0a4ffb
Cedric Lee <leeced@umich.edu>
2020-06-17 16:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
+  let animal = myAnimal();
   let yourAnimal = "cat";
+  
   return animal;
 }
 
